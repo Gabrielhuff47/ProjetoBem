5820dd56b47b8788383f1ca388a5104b9f6cdcd5
gabrielq <gabrielq@bempromotora.com.br>
2024-08-09 21:58:45 -0300

    Implementação PitchController

--- SistemaART.WebApp/Controllers/PitchController.cs
+++ SistemaART.WebApp/Controllers/PitchController.cs
@@ -25,14 +25,6 @@ public class PitchController : ControllerBase
             var pitches = await _pitchService.ListarTodos();
             return Ok(pitches);
         }
-
-        // [HttpGet("{usuario}")]
-        // public async Task<IActionResult> RetornoPichPorUsuario(string usuario)
-        // {
-        //     var pitches = await _pitchService.ListarPitchPorUsuario(usuario);
-        //     return Ok(pitches);
-        // }
-
         [HttpGet("ConsultaPitchUsuario")]
         [Authorize]
         public async Task<IActionResult> ListaPitchPorUsuario()
@@ -52,34 +44,22 @@ public class PitchController : ControllerBase
         [HttpGet("ConsultaPitchPorId")]
         [Authorize]
     
-        
         public async Task<IActionResult> ListarPitchPorId(int id)
         {
             var pitchPorId = await _pitchService.ListarPitchPorId(id);
             var usuario = User.FindFirst(ClaimTypes.Name)?.Value;
             var pitch = pitchPorId.FirstOrDefault();
 
+            if (pitch == null)
+            {
+                return NotFound("pitch não existente!");
+            }
 
             if(pitch.SituacaoAndamento =="S" && pitch.UsuarioAtualizacao.Trim() == usuario.Trim())
             {
                 return Ok(pitch);
-            }
-            else 
+            } 
              return BadRequest("O pitch não permite que ÉPICO seja criado.");
         }
-  
- 
-  
-  
     }   
-
-        
-
-        //     [HttpGet("{id}")]
-        // public async Task<IActionResult> ListarPitchPorId()
-        // {
-        //     var usuarioId = int.Parse(User.FindFirst(JwtRegisteredClaimNames.Sub)?.Value);
-        //     var pitches = await _pitchService.ListarPitchPorId(usuarioId);
-        //     return Ok(pitches);
-        // }
 
